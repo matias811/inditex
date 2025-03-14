7117f1f171fc8b3745d57d0decc260a4b769aa40
Matias <matiassarrailh@gmail.com>
2025-03-14 20:39:08 +0100

    chore: Setup of spring boot project and h2 on memory db config

---
+++ src/test/java/com/inditex/core/platform/ecommerce/app/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.core.platform.ecommerce.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
